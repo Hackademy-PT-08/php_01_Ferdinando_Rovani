ee271cb346dc506fc2996083e9508260adf96ee7
Frovani91 <ferdinando.rovani@live.it>
2023-06-27 21:31:03 +0200

    esercizio3

--- index.php
+++ index.php
@@ -32,4 +32,79 @@ var_dump(STR);
 var_dump(BOOLEANS);
 
 
+// esercizio 2
+
+// $1text = "Marco"; non è consentito inziare a dichiarare la variabile con un numero
+// $text2 = "hai"; ok
+// $text.3 = "sete"; il "." non è consentito come carattere
+// $text4 = "?"; ok
+// @text5 = "Perchè"; la variabile deve iniziare con il "$"
+// $te-xt6 = '$text2'; il "-" non è consentito come carattere e per richiamare variabile servono i doppi apici
+// $text 7 = 'bevuto'; non si possono inserire spazi nei nomi delle variabili
+// $text8 = "tutto" manca il ";" finale
+
+// CORRETTE
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = "$text2";
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+// “Marco Hai sete? Perche' hai bevuto tutto.“
+
+var_dump($text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text6 . " " . $text7 . " " . $text8);
+
+// esercizio 3
+
+
+$words1 = 
+[
+    'una',
+    67,
+    'vita',
+    'colle',
+    'mi',
+    'rosso',
+    [
+        'oscura',
+        'era',
+        89,
+        [
+            'mezzo',
+            [
+                'cammin',
+                'Nel',
+                [
+                    'selva',
+                    'la',
+                    [
+                        'via',
+                        'una',
+                        true,
+                    ]
+                ],
+            ]
+        ],
+        'ritrovai',
+        'per'
+    ],
+    'diritta'
+];
+  
+$words2 = 
+[
+    'elemento1' => 25.89,
+    'elemento2' => 'nostra',
+    'elemento3' => 
+    [
+        'Virgilio',
+        'smarrita',
+        'ché'
+    ]
+];
+
+  var_dump($words1[6][3][1][1] . " " . $words1[6][3][0] . " del " . $words1[6][3][1][0] . " di " . $words2["elemento2"] . " " . $words1[2] . " " . $words1[4] . " " . $words1[6][4] . " in " . $words1[6][3][1][2][2][1] . " " . $words1[6][3][1][2][0] . " " . $words1[6][0] . " " . $words2["elemento3"][2]. " " . $words1[6][3][1][2][1] . " " . $words1[7]. " " . $words1[6][3][1][2][2][0] . " " . $words1[6][1] . " " . $words2["elemento3"][1]);
 

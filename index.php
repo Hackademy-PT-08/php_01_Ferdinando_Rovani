920d297c3ca0a0ac5ec935ffed01c7b00926a31f
Frovani91 <ferdinando.rovani@live.it>
2023-06-27 20:01:08 +0200

    esercizio1

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+// esercizio 1
+// Definisci 4 variabili:
+
+//     - Integer 
+//     - Float 
+//     - String 
+//     - Boolean
+
+// A schermo, fai comparire il tipo di dato delle varie variabili.
+// Trasforma quelle variabili in costanti, utilizzando le best practice viste a lezione.
+
+$number = 1;
+$float = 1.2;
+$str = "stringa";
+$boolean = true;
+
+var_dump ($number);
+var_dump ($float);
+var_dump($str);
+var_dump($boolean);
+
+const NUMBER = 1;
+const FLOATS = 1.2;
+const STR = "stringa";
+const BOOLEANS = true;
+
+var_dump (NUMBER);
+var_dump (FLOATS);
+var_dump(STR);
+var_dump(BOOLEANS);
+
+
+
